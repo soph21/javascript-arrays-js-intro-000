dd7405ff91d7031336ae8eb21dc1f1dcb82c2b73
Sophie Shin <sophie@alloy.co>
2021-02-22 23:29:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,7 @@ function accessElementInArray(array, index){
   function removeElementFromBeginningOfArray(array){
     return array.slice(1)
   }
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return[...array]
+}
